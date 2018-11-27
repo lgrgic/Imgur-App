5a176459efcd96eb1c41d4db0a012c9fad4f2383
lgrgic <lukagrgic14@gmail.com>
2018-11-27 16:12:06 +0100

    add styles, post and gallery list

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+import galleriesList from "./gallerieslist";
+import galleriesFilter from "./filter";
+import comments from "./comments";
+export default combineReducers({
+  galleriesList,
+  galleriesFilter,
+  comments
+});
